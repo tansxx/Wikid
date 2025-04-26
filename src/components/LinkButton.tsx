f01c1285f15ea8c9e69ddcc404e8e122a441123a
prkhaeun <hani00415@naver.com>
2025-04-26 16:24:42 +0900

    feat : 링크 버튼 컴포넌트 작성

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,38 @@
+import styles from './LinkButton.module.css';
+import Image from 'next/image';
+import { FC } from 'react';
+
+interface LinkButtonProps {
+  label: string;
+  link: string;
+  textColor?: string;
+}
+
+const LinkButton: FC<LinkButtonProps> = ({ label, link, textColor = '#4CBFA4' }) => {
+  const handleClick = async () => {
+    try {
+      await navigator.clipboard.writeText(link);
+      alert('링크가 복사되었습니다!');
+    } catch (err) {
+      console.error('복사 실패:', err);
+    }
+  };
+
+  return (
+    <button
+      className={styles.linkButton}
+      onClick={handleClick}
+      style={{ color: textColor }}
+    >
+      <Image
+        src="/type=link.png"
+        alt="링크 아이콘"
+        width={20}
+        height={20}
+      />
+      {label}
+    </button>
+  );
+};
+
+export default LinkButton;
